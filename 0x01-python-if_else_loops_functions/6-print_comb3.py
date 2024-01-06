a304774448bbd02a46ed763c210d0ed300be1389
bossambani <bossambaka@gmail.com>
2024-01-05 20:55:50 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print(f"{tens_digit}{ones_digit}", end=", " if tens_digit < 9 or ones_digit               < 9 else "\n")
+for i in range(10):
+    for j in range(i + 1, 10):
+        #if i == 8 and j == 9:
+         #   print('89')
+        #else:
+            print('{}{}, '.format(i, j), end='')
