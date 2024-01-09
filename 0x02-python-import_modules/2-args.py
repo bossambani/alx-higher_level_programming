f62e3c09e093cbc9a9867f35e9e39fe2372cfbcb
BOSS AMBAKA AMBANI <123232647+bossambani@users.noreply.github.com>
2024-01-09 14:17:46 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
 if __name__ == "__main__":
@@ -14,6 +13,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-=======
 
->>>>>>> e11516b35a7564c7d3c89db0b87aebb40579ba86
