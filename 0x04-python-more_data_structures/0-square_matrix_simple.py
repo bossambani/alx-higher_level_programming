952e73ddf6d84dca52651ef9ddd676cd786ecbb2
bossambani <bossambaka@gmail.com>
2024-03-20 13:20:24 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]): 
+def square_matrix_simple(matrix=[]):
     new_matrix = []
     for col in matrix:
         result = list(map(lambda x: x**2, col))
