ebf45554b3c0f1d469bf92fa1fab5e39a24f3259
bossambani <bossambaka@gmail.com>
2024-04-07 23:58:37 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """
+    A class representing a square.
+    Attributes:
+    __size(int): The size of the square.
+    """
+    def __init__(self, size):
+        """
+
+        Initialize a sauare object with a given size.
+
+        """
+        self.__size = size
