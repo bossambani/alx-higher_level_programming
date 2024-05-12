ef60c63eed148cfc0bad720b92fa925a206dabb6
bossambani <bossambaka@gmail.com>
2024-05-12 05:47:09 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines a function that writes to a file"""
 
+
 def write_file(filename="", text=""):
     with open(filename, "w", encoding='utf-8') as file:
         file.write(text)
-        return len(text)   
+        return len(text)
