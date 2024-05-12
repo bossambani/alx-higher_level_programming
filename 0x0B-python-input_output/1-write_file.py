ec4adb65e5ca8fd53f542b087ed5aa560a6c3bc8
bossambani <bossambaka@gmail.com>
2024-05-12 05:52:50 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,12 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding='utf-8') as file:
+    """
+        args: filename is the name of the file to be written onto
+              text is the text to be written on the file
+
+        Returns: The number of characters written
+    """
+    with open(filename, mode="w", encoding='utf-8') as file:
         file.write(text)
-        return len(text)
+    return len(text)
