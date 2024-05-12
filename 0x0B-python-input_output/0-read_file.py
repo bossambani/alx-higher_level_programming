0e6e082feaaa85586096e1e45d26247ad10c2460
bossambani <bossambaka@gmail.com>
2024-05-12 05:36:41 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
+"""read file function module"""
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding = "utf-8") as file:
+    """
+        Args: filename is the name of the file to be read
+
+        Returns: prints the file content
+    """
+    with open(filename, "r", encoding="utf-8") as file:
         for line in file:
             print(line, end="")
