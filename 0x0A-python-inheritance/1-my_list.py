b3626c0cf8f7bb2d8198bbc2844df7c6f94bfb51
bossambani <bossambaka@gmail.com>
2024-05-13 09:17:45 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,8 +5,8 @@
 class MyList(list):
 
     def print_sorted(self):
-    """
-        Args: MyList is the subclass of list
-    """
+        """
+            Args: MyList is the subclass of list
+        """
         sorted_list = sorted(self)
         print(sorted_list)
