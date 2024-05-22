a05818b1d377d70acd760a88f4a4f813e3a6218d
bossambani <bossambaka@gmail.com>
2024-05-22 10:03:54 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.id = states.id
-ORDER BY id ASC;
+SELECT a.id AS id, a.name AS name, b.name AS name
+FROM cities a
+INNER JOIN states b ON a.state_id = b.id
+ORDER BY a.id ASC;
