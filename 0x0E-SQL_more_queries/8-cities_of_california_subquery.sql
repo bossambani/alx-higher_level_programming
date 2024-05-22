12d936628634182fec5c42aca1d739a8c62676d6
bossambani <bossambaka@gmail.com>
2024-05-22 09:43:13 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,9 +2,9 @@
 USE hbtn_0d_usa;
 SELECT *
 FROM cities
-WHERE state_id =
-	(SELECT id
+WHERE state_id =(
+	SELECT id
 	FROM states
 	WHERE name = 'California'
-	)
-ORDER BY id ASC
+)
+ORDER BY id ASC;
