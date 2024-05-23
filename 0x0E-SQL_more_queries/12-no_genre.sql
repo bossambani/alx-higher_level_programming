53c32d663d3dd50c6dd4fc0a0e44e2aeb3d80976
bossambani <bossambaka@gmail.com>
2024-05-22 19:12:29 -0700

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- a script that lists all shows contained in hbtn_0d_tvshows.
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT b.title, a.genre_id
 FROM tv_show_genres a
 RIGHT JOIN tv_shows b
