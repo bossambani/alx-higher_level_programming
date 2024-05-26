796655168736d30b8a2fe7d0cc4836c62b18ff9f
bossambani <bossambaka@gmail.com>
2024-05-26 07:20:54 -0700

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+from models.base import Base
+"""Defines a class"""
+
+
+class Rectangle(Base):
+    """Defines class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the class with the set attributes"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x attribute"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the x attribute"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for the y attribute"""
+        self.__y = value
