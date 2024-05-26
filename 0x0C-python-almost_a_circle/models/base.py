158f203e2a82929c7b30bbc8baad34b538bbf3ac
bossambani <bossambaka@gmail.com>
2024-05-26 05:44:16 -0700

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" define a class"""
+
+
+class Base:
+    """Defines class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes the Base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
