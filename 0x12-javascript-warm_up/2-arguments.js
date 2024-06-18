3ba87798c75ef1aad59ff43e54b9f421d9625834
bossambani <bossambaka@gmail.com>
2024-06-18 03:01:19 +0000

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const numArgs = process.argv.length - 2;
 if (numArgs === 0) {
   console.log('No argument');
 } else if (numArgs === 1) {
-  sonsole.log('Argument found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
