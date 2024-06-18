bee6a70fb65e3f4b8f63cf92ccc568e92b49ce57
bossambani <bossambaka@gmail.com>
2024-06-18 02:23:19 +0000

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (isNaN(arg1) || isNaN(arg2)) {
+  console.log('NaN');
+} else {
+  add(arg1, arg2);
+}
