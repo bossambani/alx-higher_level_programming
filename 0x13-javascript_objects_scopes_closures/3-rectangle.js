f9b0c78fb9de425e8c23b2e2a68b6a1c9f17d053
bossambani <bossambaka@gmail.com>
2024-06-19 00:30:39 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,6 +4,9 @@ class Rectangle {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
     }
   }
 
