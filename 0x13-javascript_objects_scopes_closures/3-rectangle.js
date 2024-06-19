24cf27f105c7b865bb71f61798240feaa117da29
bossambani <bossambaka@gmail.com>
2024-06-19 00:23:10 +0000

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('#'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
