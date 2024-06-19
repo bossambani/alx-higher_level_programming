54b57e42127baf6e22840c9c1f2cc53e32d7f989
bossambani <bossambaka@gmail.com>
2024-06-19 02:44:06 +0000

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-return function convertToBase(number) {
-return number === 0 ? '0' :
-number < 0 ? '-' + convertToBase(-number):
-convertToBase(Math.floor(number / base)) + (number % base).toString(base);
-};
+  return function convertToBase (number) {
+    return number === 0
+      ? '0'
+      : number < 0
+        ? '-' + convertToBase(-number)
+        : convertToBase(Math.floor(number / base)) + (number % base).toString(base);
+  };
 };
