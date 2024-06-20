e253c1d1cd49b5f8029867dc44cc980c2e26a331
bossambani <bossambaka@gmail.com>
2024-06-20 16:25:07 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+"""script that takes in an argument and displays all values in the state
+   table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+def list_state_name_searched(username, password, dbname, searched_state):
+    """connects to the database"""
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=dbname
+            )
+
+    try:
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM states WHERE name = %s"
+
+        cursor.execute(query, (searched_state,))
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+    finally:
+        cursor.close()
+        connection.close()
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    searched_state = sys.argv[4]
+
+    list_state_name_searched(username, password, dbname, searched_state)
