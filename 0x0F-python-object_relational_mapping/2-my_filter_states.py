1dc0b2ab015e6dc212cd90c754b4aa090da2f201
bossambani <bossambaka@gmail.com>
2024-06-22 00:36:06 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-"""script that takes in an argument and displays all values in the state
-   table of hbtn_0e_0_usa where name matches the argument.
-"""
 
-
-def list_state_name_searched(username, password, dbname, searched_state):
-    """connects to the database"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
     connection = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=username,
-            passwd=password,
-            db=dbname
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
             )
 
     cursor = connection.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s"
+    searched_state = sys.argv[4]
 
-    value = f"{searched_state}"
-
-    cursor.execute(query, (value,))
+    cursor.execute(
+            "SELECT * FROM states WHERE name = %s" % f"'{searched_state}'"
+            )
 
     rows = cursor.fetchall()
 
@@ -32,13 +27,3 @@ def list_state_name_searched(username, password, dbname, searched_state):
 
     cursor.close()
     connection.close()
-
-
-if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-    searched_state = sys.argv[4]
-
-    list_state_name_searched(username, password, dbname, searched_state)
