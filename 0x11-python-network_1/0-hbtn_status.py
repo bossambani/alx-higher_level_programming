7275ac8c09a29900b54387fcc05282fed5a0ca64
bossambani <bossambaka@gmail.com>
2024-07-05 15:43:50 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#Python script that fetches https://alx-intranet.hbtn.io/status
+# Python script that fetches https://alx-intranet.hbtn.io/status
+
 if __name__ == "__main__":
     import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
