e46f5f0d22797a9dbe15faf4088a9f6fa69cc244
bossambani <bossambaka@gmail.com>
2024-07-05 15:48:46 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         data = response.read()
-        print('Body response')
+        print('Body response:')
         print('\t- type:', type(data))
         print('\t- content:', data)
         print('\t- utf8 content:', data.decode('utf-8'))
