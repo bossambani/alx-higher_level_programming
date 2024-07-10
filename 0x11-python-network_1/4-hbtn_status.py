e47bc0c6b92488b9af06ff2be71b97130ace23d2
bossambani <bossambaka@gmail.com>
2024-07-09 19:55:15 -0700

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 import requests
 if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
-    print('Body response;')
+    print('Body response:')
     print('\t- type: ', type(req.text))
     print('\t- content: ', req.text)
