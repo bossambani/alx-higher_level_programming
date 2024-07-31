1d2d0746a1c046a63e10059ca305861da68f5269
bossambani <bossambaka@gmail.com>
2024-07-30 19:50:43 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
-// Import the request module
+// import the module
 const request = require('request');
 
+// The first argument is the movie ID
 const url = process.argv[2];
-const wedgeAntillesId = 18;
 
-request(url, (error, response, body) => {
+// Make an HTTP GET request to the specified URL
+request(url, function (error, response, body) {
+  // check for errors during the request
   if (error) {
     console.error(error);
   } else {
-    const data = JSON.parse(body);
-    let count = 0;
-
-    data.results.forEach(film => {
-      if (film.characters && film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(`${count}`);
+    const content = JSON.parse(body).results;
+    const moviesWithWedge = content.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0);
+    console.log(moviesWithWedge);
   }
 });
