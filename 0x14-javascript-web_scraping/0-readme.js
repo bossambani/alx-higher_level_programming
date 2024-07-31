fd41e08e54cb2b9185e2c2f465e99d690618028d
= <=>
2024-07-30 18:13:36 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
+
+//import the module
 const fs = require('fs');
 
+//assigns the file path to the variable
 const file = process.argv[2];
+
+//read the file
 
 try {
         const data = fs.readFileSync(file, 'utf-8');
