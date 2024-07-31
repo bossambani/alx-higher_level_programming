92aa65296dcf4e5945efe798b0c05442abee351a
bossambani <bossambaka@gmail.com>
2024-07-30 18:54:35 -0700

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+//imports the request module
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+request(url, (error, response, body) => {
+	if (error) {
+		console.error(error);
+		return;
+	}else{
+		const data = JSON.parse(body);
+		console.log(data.title);
+	}
+});
